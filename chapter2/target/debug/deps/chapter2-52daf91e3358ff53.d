dab37e742cbeadedf6e3619f6785e364e247dced
JoseFernando7 <jose.f72000@gmail.com>
2023-07-21 23:44:37 -0500

    Chapter 2

--- chapter2/target/debug/deps/chapter2-52daf91e3358ff53.d
+++
@@ -1,5 +0,0 @@
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-52daf91e3358ff53.rmeta: src/main.rs
-
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-52daf91e3358ff53.d: src/main.rs
-
-src/main.rs:
--- chapter2/target/debug/deps/chapter2-d19ca9793ca423cf.d
+++
@@ -1,5 +0,0 @@
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-d19ca9793ca423cf.rmeta: src/main.rs
-
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-d19ca9793ca423cf.d: src/main.rs
-
-src/main.rs:
