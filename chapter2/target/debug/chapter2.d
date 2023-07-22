bc90461b5aec3f3a0c38ccb2109588c9466e5dd1
Jose Fernando Restrepo Sanchez <jose.f72000@gmail.com>
2023-07-21 23:47:26 -0500

    Delete chapter2/target directory

--- chapter2/target/debug/chapter2.d
+++
@@ -1,1 +0,0 @@
-/home/josef7/Programacion/cosas/Rust/The\ Rust\ Programming\ Language/chapter2/target/debug/chapter2: /home/josef7/Programacion/cosas/Rust/The\ Rust\ Programming\ Language/chapter2/src/main.rs
--- chapter2/target/debug/deps/chapter2-e75ff9e42c8e30fa.d
+++
@@ -1,5 +0,0 @@
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-e75ff9e42c8e30fa: src/main.rs
-
-/home/josef7/Programacion/cosas/Rust/The Rust Programming Language/chapter2/target/debug/deps/chapter2-e75ff9e42c8e30fa.d: src/main.rs
-
-src/main.rs:
